6b6f104c050a8dff08c595365342155fe63427b5
Felipe <ficechin@hotmail.com>
2022-03-15 22:50:45 -0300

    Project admin template developing.

---
+++ admin-template/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./src/pages/**/*.{html,js,tsx,ts,jsx}",
+    "./src/components/**/*.{html,js,tsx,ts,jsx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
