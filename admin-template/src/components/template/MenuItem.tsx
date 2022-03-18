5164cb9ff72c8c41fe245930a02f2f2b5845299a
Felipe <ficechin@hotmail.com>
2022-03-17 22:09:32 -0300

    admin template developing

--- admin-template/src/components/template/MenuItem.tsx
+++ admin-template/src/components/template/MenuItem.tsx
@@ -1,23 +1,34 @@
 import Link from "next/link"
 
 interface MenuItemProps {
-    url: string
+    url?: string
     texto: string
     icone: any
+    onClick?: (evento: any) => void
+    className?: string
 }
 
 export default function MenuItem(props: MenuItemProps) {
-    return (
-        <li className="hover:bg-gray-100">
-            <Link href={props.url}>
-                <a className="flex flex-col justify-center items-center w-20 h-20">
-                    {props.icone}
-                    <span className="text-xs font-light text-gray-600">
-                        {props.texto}
-                    </span>
-                </a>
-            </Link>
+    function renderizarLink() {
+        return (
+            <a className={"flex flex-col justify-center items-center w-20 h-20 dark:text-gray-200 " + props.className}>
+                {props.icone}
+                <span className="text-xs font-light">
+                    {props.texto}
+                </span>
+            </a>
+        )
+    }
 
+    return (
+        <li onClick={props.onClick} className="hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer">
+            {props.url ? (
+                <Link href={props.url}>
+                    {renderizarLink()}
+                </Link>
+            ) : (
+                renderizarLink()
+            )}
         </li>
     )
 }
